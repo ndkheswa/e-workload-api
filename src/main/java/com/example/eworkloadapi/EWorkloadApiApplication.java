4f588aa058742f104d480574646ec032320bf5ca
Ndoda Kheswa (ZA) <Ndoda.Kheswa@absa.africa>
2020-03-17 15:39:53 +0200

    initial commit

---
+++ src/main/java/com/example/eworkloadapi/EWorkloadApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.eworkloadapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EWorkloadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EWorkloadApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eworkloadapi/EWorkloadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eworkloadapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EWorkloadApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
